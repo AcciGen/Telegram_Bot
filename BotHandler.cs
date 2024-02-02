ab3a97213b76695ecc5ac9b2c1f42b987cbb104a
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-02-02 07:55:42 +0500

    Bot add BotHandler Class

---
+++ BotHandler.cs
@@ -0,0 +1,66 @@
+﻿
+
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot;
+using System.Reflection.Metadata;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+
+namespace Telegram_Bot
+{
+    public class BotHandler
+    {
+        public string botToken { get; set; }
+
+        public BotHandler(string token)
+        {
+            botToken = token;
+        }
+
+
+        public async Task BotHandle()
+        {
+            var botClient = new TelegramBotClient(botToken);
+
+            using CancellationTokenSource cts = new();
+
+            ReceiverOptions receiverOptions = new()
+            {
+                AllowedUpdates = Array.Empty<UpdateType>()
+            };
+
+            botClient.StartReceiving(
+                updateHandler: HandleUpdateAsync,
+                pollingErrorHandler: HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token
+            );
+
+            var me = await botClient.GetMeAsync();
+
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+
+            cts.Cancel();
+        }
+
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient,Update update, CancellationToken cancellationToken)
+        {
+
+        }
+
+
+        async Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+        }
+    }
+}
